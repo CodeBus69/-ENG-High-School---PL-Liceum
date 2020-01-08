56c0617f65020171d76639feb52f157731c54687
CodeBus69 <kucharczyk.main@gmail.com>
2020-01-08 21:38:22 +0100

    Add new files

---
+++ Zadanie 3.22/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int sum=0,product=1,tab[10];
+    srand(time(NULL));
+    for (int i=0; i<10; i++) {
+        system ("cls");
+        tab[i]=rand()%34-10;
+        sum+=tab[i];
+        if(tab[i]<6) product*=tab[i];
+        cout << "Ostatnia wylosowana liczba: "<< tab[i] << endl
+        << "Suma: " << sum << endl
+        << "Ilocyzn mniejszych od 6: " << product << endl;
+    }
+    return 0;
+}
